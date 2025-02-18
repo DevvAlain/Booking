ce56d1ffa494c471143e42a87aa0a8f7d73ffaab
Khắc Bảo <caokhacbao0712@gmail.com>
2025-02-18 15:50:52 +0700

    done crud product,category

--- src/models/product.js
+++ src/models/product.js
@@ -1,32 +1,23 @@
 'use strict';
-const {
-    Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class Product extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // define association here
-        }
-    };
-    Product.init({
-        name: DataTypes.STRING,
-        description: DataTypes.STRING,
-        image_url: DataTypes.STRING,
-        date: DataTypes.BIGINT,
-        price: DataTypes.FLOAT
-    },
-        {
-            sequelize,
-            modelName: 'Product',
-            tableName: "products",
-            freezeTableName: true,
-
-        }
-    );
-    return Product;
+  class Product extends Model {
+    static associate(models) {
+      Product.belongsTo(models.Category, { foreignKey: 'categoryId', as: 'category' });
+    }
+  };
+  Product.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    image_url: DataTypes.STRING,
+    date: DataTypes.BIGINT,
+    price: DataTypes.FLOAT,
+    categoryId: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'Product',
+    tableName: 'products',
+    freezeTableName: true,
+  });
+  return Product;
 };
